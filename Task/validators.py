a3499ad4be29cfc058d9938d96a22e6b05dd9e69
AliReza7222 <ali.r.salehi99@gmail.com>
2023-12-01 17:08:52 +0330

    update validators check task's title

--- Task/validators.py
+++ Task/validators.py
@@ -4,8 +4,11 @@ from django.core.exceptions import ValidationError
 
 # This validator checked the text: contains words and numbers and _ but starts with words.
 def check_text(value):
+    # check the value against the regex pattern. If the pattern does not match, regex_check is 0
     regex_check = re.match('^[^\d_][a-zA-Z_\d]+', value) or 0
     if regex_check:
         return value
-    raise ValidationError(f'Invalid value .')
+    raise ValidationError(
+        'Title is invalid Please enter a valid title that starts with words and contains words, numbers, and \'_\' '
+    )
 
