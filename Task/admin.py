78baf4b81f597930b7703b56b4166124a31d4bf3
AliReza7222 <ali.r.salehi99@gmail.com>
2023-12-01 17:02:23 +0330

    add actions mark to complete or not complete

--- Task/admin.py
+++ Task/admin.py
@@ -12,6 +12,33 @@ class TaskAdmin(admin.ModelAdmin):
         'due_date',
         'completed'
     ]
-    list_display = ['title', 'due_date', 'completed']
+    list_display = [
+        'title',
+        'due_date',
+        'completed'
+    ]
     list_filter = ['completed']
     sortable_by = ['due_date']
+    actions = [
+        'mark_to_completed',
+        'mark_to_not_completed'
+    ]
+
+    # action for mark to completed task .
+    def mark_to_completed(self, request, queryset):
+        updated = queryset.update(completed=True)
+        self.message_user(
+            request, f'{updated} mark to completed task .'
+        )
+
+    # action for mark to not completed task
+    def mark_to_not_completed(self, request, queryset):
+        updated = queryset.update(completed=False)
+        self.message_user(
+            request, f'{updated} mark to not completed task .'
+        )
+
+    # short description view in admin panel
+    mark_to_not_completed.short_description = 'mark task for not completion'
+    mark_to_completed.short_description = 'mark task for completion'
+
