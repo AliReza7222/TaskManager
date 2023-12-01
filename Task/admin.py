98a06bf77b1d08cca752a226cc577f36afa823d8
AliReza7222 <ali.r.salehi99@gmail.com>
2023-12-01 16:31:46 +0330

    create a custom admin panel for app task and files urls add to apps task and accounts

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+# registered the Task model to admin panel and created a custom admin panel
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'description',
+        'due_date',
+        'completed'
+    ]
+    list_display = ['title', 'due_date', 'completed']
+    list_filter = ['completed']
+    sortable_by = ['due_date']
